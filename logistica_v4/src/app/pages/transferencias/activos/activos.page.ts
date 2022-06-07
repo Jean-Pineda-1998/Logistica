544a6f8acd257c1259aa168ecdb2d97766ce3319
Jean-Pineda-1998 <gpineda@sywork.net>
2022-06-07 16:05:20 -0500

    Se agrega al proyecto correccion de botones

--- logistica_v4/src/app/pages/transferencias/activos/activos.page.ts
+++ logistica_v4/src/app/pages/transferencias/activos/activos.page.ts
@@ -14,17 +14,34 @@ export class ActivosPage implements OnInit {
   code3: any;
   code4: any;
 
-  constructor( private barcodeScanner: BarcodeScanner  ) { }
+  constructor( private barcodeScanner: BarcodeScanner  ) {
+
+  }
 
   ngOnInit() {
   }
+
+
+  scanBarCode(id) {
+
+    this.barcodeScanner.scan().then(barcodeData => {
+        for (var i = 0; i < this.main2.length; i++) {
+            if (this.main2[i].id == id) {
+                this.main2[i].scannedBarCode = barcodeData.text;
+                break;
+            }
+        }
+    }, (err) => {
+        console.log('Error: ', err);
+    });
+}
 
 
   scan0(){
 
     this.barcodeScanner.scan().then(barcodeData => {
       this.code0 = barcodeData.text;
-      console.log('Barcode data', this.code0);
+      // console.log('Barcode data', this.code0);
 
      }).catch(err => {
          console.log('Error', err);
@@ -34,7 +51,7 @@ export class ActivosPage implements OnInit {
 
     this.barcodeScanner.scan().then(barcodeData => {
       this.code1 = barcodeData.text;
-      console.log('Barcode data', this.code1);
+      // console.log('Barcode data', this.code1);
 
      }).catch(err => {
          console.log('Error', err);
@@ -44,7 +61,7 @@ export class ActivosPage implements OnInit {
 
     this.barcodeScanner.scan().then(barcodeData => {
       this.code2 = barcodeData.text;
-      console.log('Barcode data', this.code2);
+      // console.log('Barcode data', this.code2);
 
      }).catch(err => {
          console.log('Error', err);
@@ -54,7 +71,7 @@ export class ActivosPage implements OnInit {
 
     this.barcodeScanner.scan().then(barcodeData => {
       this.code3 = barcodeData.text;
-      console.log('Barcode data', this.code3);
+      // console.log('Barcode data', this.code3);
 
      }).catch(err => {
          console.log('Error', err);
